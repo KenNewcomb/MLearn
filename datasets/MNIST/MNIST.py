505e7e0369bbfc8204fa1b93ef7e66bc3744eba2
Ken Newcomb <KenNewcomb@protonmail.com>
2020-02-08 21:55:41 -0500

    Added MNIST data import tool, added requirements.txt.
    Former-commit-id: 3c0018a4cf445f2ea3ef70911e2636a6603a63f0

---
+++ datasets/MNIST/MNIST.py
@@ -0,0 +1,21 @@
+'''MNIST.py: A module for importing MNIST data.'''
+
+X_train = []
+y_train = []
+def load_data():
+    data = []
+    with open('datasets/MNIST/mnist_train_part1.csv', 'r') as f:
+        data.extend(f.readlines())
+    with open('datasets/MNIST/mnist_train_part2.csv', 'r') as f:
+        data.extend(f.readlines())
+
+    # Extract X and Y
+    print("Loading Y training data...")
+    y_train = [int(i.split(',')[0]) for i in data]
+    print("Loading X training data...")
+    X_train = [i.split(',')[1:] for i in data]
+    X_train = [[int(j) for j in i] for i in X_train]
+
+    print("Data loaded successfully.")
+    
+    return X_train, y_train
