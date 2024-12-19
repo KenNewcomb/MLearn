bb543e95ab959002731395056bdb188ecec361c0
Ken Newcomb <KenNewcomb@protonmail.com>
2024-12-18 19:23:05 -0500

    refactor: Improve Voting class documentation and handle empty predictions in majority voting method

--- algorithms/supervised/ensemble.py
+++ algorithms/supervised/ensemble.py
@@ -1,13 +1,23 @@
-from math import mode
+from statistics import mode
 class Voting:
-    # TODO Describe what this class does
+    """
+    A class that implements a voting ensemble method for combining predictions from multiple models.
+    """
 
     def __init__(self, models):
+        """
+        Initialize the Voting class with a list of models.
+
+        Parameters:
+        models (list): A list of models that have a predict method.
+        """
         self.models = models
 
     def majority_voting(self, X):
         predictions = []
-        for model in models:
+        for model in self.models:
             predictions.append(model.predict(X))
+        if not predictions:
+            raise ValueError("No predictions were made by the models.")
         majority = mode(predictions)
         return majority
